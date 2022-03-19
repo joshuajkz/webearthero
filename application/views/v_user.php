f557c59bbfa5a6c65dfa16538a6da2fdd6c0ccc1
joshuajkz <joshuakurniawanx@gmail.com>
2022-03-20 02:01:58 +0700

    #6 | 08.20

---
+++ application/views/v_user.php
@@ -0,0 +1,18 @@
+<div class="col-md-12">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Data User</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-primary btn-xs"><i class="fas fa-plus"></i>Add</button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                The body of the card
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+</div>
