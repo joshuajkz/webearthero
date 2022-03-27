a181bbc70c89c905fff772b4bfac2af9ceb6cfd7
joshuajkz <joshuakurniawanx@gmail.com>
2022-03-28 00:21:46 +0700

    #9 CRUD User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -8,7 +8,8 @@ class User extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        //Load Dependencies
+        $this->load->model('m_user');
+        
 
     }
 
@@ -17,6 +18,7 @@ class User extends CI_Controller {
     {
         $data = array(
             'title' => 'User',
+            'user' => $this->m_user->get_all_data(),
             'isi' => 'v_user',
         );
         $this->load->view('layout/v_wrapper_backend', $data, FALSE);
@@ -25,19 +27,40 @@ class User extends CI_Controller {
     // Add a new item
     public function add()
     {
-
+        $data = array(
+            'nama_user' => $this->input->post('nama_user'),
+            'username' => $this->input->post('username'),
+            'password' => $this->input->post('password'),
+            'level_user' => $this->input->post('level_user'),
+         );
+         $this->m_user->add($data);
+         $this->session->set_flashdata('pesan', 'Data Berhasil Ditambahkan');
+         redirect('user');
+         
     }
 
     //Update one item
-    public function update( $id = NULL )
+    public function edit ( $id_user = NULL )
     {
-
+        $data = array(
+            'id_user' => $id_user,
+            'nama_user' => $this->input->post('nama_user'),
+            'username' => $this->input->post('username'),
+            'password' => $this->input->post('password'),
+            'level_user' => $this->input->post('level_user'),
+         );
+         $this->m_user->edit($data);
+         $this->session->set_flashdata('pesan', 'Data Berhasil Diedit');
+         redirect('user');
     }
 
     //Delete one item
-    public function delete( $id = NULL )
+    public function delete( $id_user = NULL )
     {
-
+        $data = array('id_user' => $id_user);
+        $this->m_user->delete($data);
+        $this->session->set_flashdata('pesan', 'Data Berhasil Dihapus');
+        redirect('user');
     }
 }
 
--- application/views/layout/v_nav_backend.php
+++ application/views/layout/v_nav_backend.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="<?= base_url('admin')?>" class="brand-link">
       <img src="<?= base_url()?>template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Halaman Admin</span>
@@ -15,7 +15,7 @@
           <img src="<?= base_url()?>template/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?= $this->session->userdata('nama_user')?></a>
         </div>
       </div>
 
@@ -25,7 +25,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p> Dashboard </p>
             </a>
@@ -72,7 +72,9 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('user')?>" class="nav-link">
+            <a href="<?= base_url('user')?>" class="nav-link <?php if ($this->uri->segment(1)=='user') {
+              echo "active";
+            }?>">
               <i class="nav-icon fas fa-users"></i>
               <p> User </p>
             </a>
